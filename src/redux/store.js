91fdd3234ee0d2d55186ead3eb3be306f85a440a
Saad <saadullahkhx@gmail.com>
2021-02-10 15:39:13 +0500

    final commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,17 @@
-import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
-import rootReducer from "./root.reducer";
-import logger from "redux-logger";
-import { persistStore } from "redux-persist";
-import { rootSagas } from "./root.sagas";
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './root.reducer';
+import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
+import { rootSagas } from './root.sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [logger, sagaMiddleware];
+const middlewares = [sagaMiddleware];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
