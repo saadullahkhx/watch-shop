48d64e0f87a3228ae335cbefa2f8fe72806a2de5
Saad <saadullahkhx@gmail.com>
2021-02-10 17:00:25 +0500

    fixed some bugs causing deployment error

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASEURL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: 'AIzaSyC6T7ShFsz7Egkm8uJln1zAi9Hf8cQn0U8',
+  authDomain: 'watch-shop-79f54.firebaseapp.com',
+  databaseURL: 'https://watch-shop-79f54.firebaseio.com',
+  projectId: 'watch-shop-79f54',
+  storageBucket: 'watch-shop-79f54.appspot.com',
+  messagingSenderId: '308363030404',
+  appId: '1:308363030404:web:4b7444a9360cddd0c837d1',
+  measurementId: 'G-PCHMZJ3S0X',
 };
 
 firebase.initializeApp(firebaseConfig);
